b91fd6dc9f9c597fdf78e7d55a24978d8f5176e4
kacpercebula <kacpercebula21@augustana.edu>
2023-03-14 20:40:41 -0500

    cleaned tournament data into raw dataset

---
+++ dataCleaning.R
@@ -0,0 +1,63 @@
+library(tidytext)
+library(textdata)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(ggplot2)
+library(wordcloud)
+library(sentimentr)
+library(reshape2)
+library(readr)
+library(shiny)
+library(DT)
+library(lubridate)
+
+rm(list = ls())
+
+##########    IMPORTANT   ################
+# before running the data cleaning program, make sure you set your directory to
+  # folder with the .csv files
+
+#  NOTES   #
+#This piece of data is the same as Tournamnet data just less team. 
+  #GameData <- read.csv("2023 Game Data.csv")
+
+#This piece of data may be difficult to use right now but we may use it later in analyzation
+  #Lina will analyze this in a different RScript possibly
+  #TournamentConferenceData <- read.csv("Tournament Conference Data.csv")
+
+#reads in all the .csv files
+GameData <- read.csv("2023 Tournament Data.csv")
+TournamentGameData <- read.csv("Tournament Game Data.csv")
+
+
+#1. cleaning for Game Data, filtering out teams that are not in the tournamnet
+cleanGameData <- GameData %>%
+  distinct(.keep_all = TRUE) %>%
+  filter(TEAM != "Rutgers", TEAM != "Wisconsin", TEAM != "Bradley", TEAM != "Liberty",
+         TEAM != "Southern Miss", TEAM != "Utah Valley", TEAM != "Toledo", 
+         TEAM != "Yale", TEAM != "UC Irvine", TEAM != "Eastern Washington", 
+         TEAM != "Texas A&M-Corpus Christi", TEAM != "Grambling", TEAM != "Youngstown State",
+         TEAM != "Morehead State")
+#cleanGameData <- cleanGameData[-57, , drop = FALSE]
+
+#2. adding teams that made the playoffs into clean data.
+
+
+
+
+#TournamentData has all the same data as Game Data just that it is in years past,
+  #we will run our analysis on this years game data (finding the best team and predicitng 
+  #rounds possibily). Then make an RScript using the old data as well and merging that into 
+  #this years predictions. 
+
+
+
+
+
+
+  
+
+
+
+
